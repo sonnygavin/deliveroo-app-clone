e79600be0647157c815ffd4d6e1e08fec139366c
Sonny Gavin <sonnygavin@yahoo.com>
2024-03-19 16:42:44 +0200

    Search section.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,8 +4,8 @@ import { useNavigation } from "@react-navigation/native";
 import {
   UserIcon,
   ChevronDownIcon,
-  SearchIcon,
-  AdjustmentsIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon,
 } from "react-native-heroicons/outline";
 
 const HomeScreen = () => {
@@ -37,8 +37,30 @@ const HomeScreen = () => {
         </View>
         <UserIcon size={35} color="#00CCBB" />
       </View>
+
+      {/* Search box */}
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+          <MagnifyingGlassIcon color="gray" size={20} />
+          <TextInput
+            placeholder="Search for a restaurant or dish.."
+            keyboardType="default"
+          />
+        </View>
+
+        <AdjustmentsVerticalIcon color="#00CCBB" />
+      </View>
     </SafeAreaView>
   );
 };
 
 export default HomeScreen;
+
+// {/* Search box */}
+// <View>
+// <View>
+//   <SearchIcon />
+// </View>
+
+// <AdjustmentsIcon size={20} color="#00CCBB" />
+// </View>
