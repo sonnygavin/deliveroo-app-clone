f3295dbccc9ec8617add1e72daf5c2485dc00043
Sonny Gavin <sonnygavin@yahoo.com>
2024-03-19 15:59:47 +0200

    Add header section.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,43 @@
-import React from "react";
-import { Text, View } from "react-native";
-import { StatusBar } from "expo-status-bar";
+import React, { useLayoutEffect } from "react";
+import { SafeAreaView, Text, View, Image, TextInput } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import {
+  UserIcon,
+  ChevronDownIcon,
+  SearchIcon,
+  AdjustmentsIcon,
+} from "react-native-heroicons/outline";
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-red-500">Home screen component!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView className="bg-white pt-5">
+      {/* Header */}
+      <View className="flex-row pb-3 items-center mx-4 space-x-2 mt-7">
+        <Image
+          source={{
+            uri: "https://www.odtap.com/wp-content/uploads/2018/10/food-delivery.jpg",
+          }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs ">Deliver Now!</Text>
+          <Text className="font-bold text-xl ">
+            current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
+        </View>
+        <UserIcon size={35} color="#00CCBB" />
+      </View>
+    </SafeAreaView>
   );
 };
 
